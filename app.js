35fd69c71469bd8c45f4cb7e1f8ac929686f1cc6
Jengas2015 <arejang@gmail.com>
2023-03-10 18:01:00 -0600

    converted to oop

--- app.js
+++ app.js
@@ -1,45 +1,52 @@
-const tileDisplay = document.querySelector('.tile-container')
-const keyboard = document.querySelector('.key-container')
 
-const keys = [
-    'Q',
-    'W',
-    'E',
-    'R',
-    'T',
-    'Y',
-    'U',
-    'I',
-    'O',
-    'P',
-    'A',
-    'S',
-    'D',
-    'F',
-    'G',
-    'H',
-    'J',
-    'K',
-    'L',
-    'ENTER',
-    'Z',
-    'X',
-    'C',
-    'V',
-    'B',
-    'N',
-    'M',
-    '<<'
-]
+const startingUI = {
+    tileDisplay: document.querySelector('.tile-container'),
+    keyboard: document.querySelector('.key-container'),
+    keys: [
+        'Q',
+        'W',
+        'E',
+        'R',
+        'T',
+        'Y',
+        'U',
+        'I',
+        'O',
+        'P',
+        'A',
+        'S',
+        'D',
+        'F',
+        'G',
+        'H',
+        'J',
+        'K',
+        'L',
+        'ENTER',
+        'Z',
+        'X',
+        'C',
+        'V',
+        'B',
+        'N',
+        'M',
+        '<<'
+    ],
 
-const handleClick = () => {
-    console.log("clicked!")
+    handleClick: () => {
+        console.log("clicked!")
+    },
+
+    forEach() {
+        this.keys.forEach(key=> {
+            const buttonElement = document.createElement('button')
+            buttonElement.textContent = key
+            buttonElement.setAttribute('id', key)
+            buttonElement.addEventListener('click', this.handleClick)
+            this.keyboard.append(buttonElement)
+        })
+    }
 }
 
-keys.forEach(key=> {
-    const buttonElement = document.createElement('button')
-    buttonElement.textContent= key
-    buttonElement.setAttribute('id', key)
-    buttonElement.addEventListener('click', handleClick)
-    keyboard.append(buttonElement)
-})
+startingUI.forEach()
+
