7a1fa6cc471cdeb43f4ef1982317ce4a9fdc6992
Jengas2015 <arejang@gmail.com>
2023-03-15 15:46:24 -0500

    modified the functions in starterUI

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 
+const wordle = 'SUPER'
+
 const startingUI = {
     tileDisplay: document.querySelector('.tile-container'),
     keyboard: document.querySelector('.key-container'),
@@ -33,20 +35,53 @@ const startingUI = {
         '<<'
     ],
 
-    handleClick: () => {
-        console.log("clicked!")
+    guessRows: [
+        ['','','','','',],
+        ['','','','','',],
+        ['','','','','',],
+        ['','','','','',],
+        ['','','','','',],
+        ['','','','','',]
+    ],
+
+    currentRow: 0,
+    currentTile: 0,
+
+    addLetter (letter) {
+        const tile = document.getElementById(`guessRow-${this.currentRow}-tile-${this.currentTile}`)
+        tile.textContent = letter
     },
+
+    handleClick (key) {
+        console.log("clicked!", key)
+        this.addLetter (key)
+    },
+
+
 
     forEach() {
         this.keys.forEach(key=> {
             const buttonElement = document.createElement('button')
             buttonElement.textContent = key
             buttonElement.setAttribute('id', key)
-            buttonElement.addEventListener('click', this.handleClick)
+            buttonElement.addEventListener('click', () => this.handleClick(key))
             this.keyboard.append(buttonElement)
         })
+        this.guessRows.forEach((guessRow, guessRowIndex) => {
+            const rowElement = document.createElement('div')
+            rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+            guessRow.forEach((guess, guessIndex)=> {
+                const tileElement = document.createElement('div')
+                tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+                tileElement.classList.add('tile')
+                rowElement.append(tileElement)
+            })
+            this.tileDisplay.append(rowElement)
+        })
     }
+
 }
+
 
 startingUI.forEach()
 
